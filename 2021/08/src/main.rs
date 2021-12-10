5b00a9fc1f66e18c4c33776473b139c014bd3ce0
ragnarok56 <kevin.nacios@gmail.com>
2021-12-09 22:32:23 -0500

    finished part 2 getting humbled by rust...  :| also my implementation/solution is probably not ideal

--- 2021/08/src/main.rs
+++ 2021/08/src/main.rs
@@ -3,6 +3,8 @@ use clap::{Arg, App};
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::collections::HashMap;
+
 
 fn main() {
     // it is really hard to just parse some args...
@@ -11,6 +13,10 @@ fn main() {
             .short("f")
             .takes_value(true)
             .required(true))
+        .arg(Arg::with_name("sum_outputs")
+            .long("sum-outputs")
+            .takes_value(false)
+            .required(false))
         .get_matches();
     let filename = matches.value_of("filename").unwrap();
 
@@ -20,19 +26,81 @@ fn main() {
         Ok(io::BufReader::new(file).lines())
     }
 
+    fn sort_string(s: String) -> String {
+        let mut str_chars: Vec<char> = s.chars().collect();
+        str_chars.sort();
+        return String::from_iter(str_chars);
+    }
+
     if let Ok(lines) = read_lines(filename) {
-        let known_digits = lines.map(|l| {
+        let result = lines.map(|l| {
             let input = l.unwrap();
             let mut inputs_iter = input.split("|");
-            let outputs: Vec<&str> = inputs_iter.nth(1).unwrap_or("").split_whitespace().collect();
+            let digits: Vec<&str> = inputs_iter.next().unwrap_or("").split_whitespace().collect();
+            let outputs: Vec<&str> = inputs_iter.next().unwrap_or("").split_whitespace().collect();
 
-            return outputs
-                .iter()
-                .map(|x| x.chars().count())
-                .filter(|x| x == &2 || x == &3 || x == &4 || x == &7)
-                .count();
+            let count_known_digits = || {
+                return outputs
+                    .iter()
+                    .map(|x| x.chars().count())
+                    .map(|x|
+                        match x {
+                            2 | 3 | 4 | 7 => x,
+                            _ => 0
+                        }
+                    )
+                    .count();
+            };
+
+            let sum_outputs = || {
+                let digit_count_map = digits
+                    .iter()
+                    .flat_map(|x| x.split("").map(str::to_owned).collect::<Vec<_>>())
+                    .fold(HashMap::new(), |mut acc, x| {
+                        *acc.entry(x).or_insert(0) += 1;
+                        acc
+                    });
+
+                let digit_identifier_map = digits
+                    .iter()
+                    .fold(HashMap::new(), |mut acc, x| {
+                        *acc.entry(sort_string(x.to_string())).or_insert(0) += x.chars().map(|c| {
+                            return digit_count_map.get(&c.to_string()).unwrap_or(&0)
+                        }).sum::<i32>();
+                        acc
+                    });
+
+                return outputs
+                    .iter()
+                    .map(|x| {
+                        let sorted_digits = sort_string(x.to_string());
+                        return match digit_identifier_map.get(&sorted_digits).unwrap() {
+                            17 => 1,
+                            25 => 7,
+                            30 => 4,
+                            34 => 2,
+                            37 => 5,
+                            39 => 3,
+                            41 => 6,
+                            42 => 0,
+                            45 => 9,
+                            49 => 8,
+                            _ => -1
+                        };
+                    })
+                    .filter(|x| x != &-1)
+                    .map(|x| x.to_string())
+                    .collect::<Vec<String>>()
+                    .join("")
+                    .parse::<usize>().unwrap_or(0);
+            };
+
+            match matches.is_present("sum_outputs") {
+                true => sum_outputs(),
+                false => count_known_digits(),
+            }
         });
 
-        println!("{:?}", known_digits.sum::<usize>());
+        println!("{:?}", result.sum::<usize>());
     }
 }
