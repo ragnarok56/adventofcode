62ce8badae6343949fa56a4de25d03c2af43e545
ragnarok56 <kevin.nacios@gmail.com>
2021-12-15 22:06:07 -0500

    cleanup code, add comments

--- 2021/14/src/main.rs
+++ 2021/14/src/main.rs
@@ -19,7 +19,6 @@ fn main() {
 
     let input = fs::read_to_string(filename).unwrap();
 
-    let mut template = Vec::new();
     let mut insertions = HashMap::new();
     let mut polymer_map = HashMap::new();
     let mut inserted_map: HashMap<String, i64> = HashMap::new();
@@ -27,11 +26,13 @@ fn main() {
     // yup, parsing strings is like, super hard
     for (index, line) in input.lines().enumerate() {
         if index == 0 {
-            template = line.chars().fold(Vec::new(), |mut acc, x| {
+            // take first line template and prefill inserted_map and polymer_map
+            let template = line.chars().fold(Vec::new(), |mut acc, x| {
                 acc.push(x);
                 *inserted_map.entry(x.to_string()).or_insert(0) += 1;
                 return acc;
             });
+            // need as vec to use windows to generate pairs for polymer_map
             for w in template.windows(2) {
                 polymer_map.entry(format!("{}{}", w[0], w[1])).or_insert(1);
             }
@@ -46,79 +47,46 @@ fn main() {
         }
     }
 
-    // let mut polymer = template.clone();
-    println!("starting polymer_map:  {:?}", polymer_map);
-    println!("starting inserted_map: {:?}", inserted_map);
-    println!("--------------------------------");
-    for i in 0..steps {
-        // let mut polymer_copy = polymer.clone();
-        // let mut cur = 0;
-        // let mut inserts = 0;
+    for _ in 0..steps {
+        // must copy map so we can modify the copy and retain original for lookups
+        // as we iterate each step
         let mut polymer_map_copy = polymer_map.clone();
+        // so many clones
         let all_keys = polymer_map.clone().into_keys().collect::<Vec<_>>();
+
         for key in all_keys {
+            // only process polymer if it "exists" and has a positive number of occurances
             if insertions.contains_key(&key) && polymer_map.get(&key).unwrap() > &0 {
                 match insertions.get(&*key) {
                     Some(&v) => {
-                        println!("found key: {:?}", key);
-                        let inserted_char = v.chars().next().unwrap();
-                        let mut key_chars = key.chars();
                         let insert_key = key.clone();
+                        // pull out how many times this polymer occured. this needs to be
+                        // added to the number of times the insert value has been added
+                        // to account for each copy.  similarly, reduce the number of occurances
+                        // of the polymer to 0 (since we just split it everywhere)
                         let num_present = polymer_map.get(&insert_key).unwrap();
-                        println!("updating {} in inserted_map with {:?} copies from {}", v, num_present, insert_key);
                         *inserted_map.entry(v.to_string()).or_insert(0) += num_present;
-                        *polymer_map_copy.entry(key.clone()).or_insert(0) -= num_present;
+                        *polymer_map_copy.entry(insert_key).or_insert(0) -= num_present;
 
+                        // now generate new copies of polymers using the original pair
+                        // so AB -> C  insert would add an AC and CB pair.  Add the same
+                        // number of these for however many of the original (AB) were in
+                        // the polymer_map
+                        let mut key_chars = key.chars();
+                        let inserted_char = v.chars().next().unwrap();
                         let key1 = format!("{}{}", key_chars.next().unwrap(), inserted_char);
                         *polymer_map_copy.entry(key1).or_insert(0) += num_present;
 
                         let key2 = format!("{}{}", inserted_char, key_chars.next().unwrap());
                         *polymer_map_copy.entry(key2).or_insert(0) += num_present;
-                        println!("inserted_map: {:?}", inserted_map);
-                        println!("polymer_map:  {:?}", polymer_map_copy);
-                        println!("--------------------------------")
                     },
                     None => ()
                 }
-                // inserts += 1;
             }
         }
         polymer_map = polymer_map_copy;
-        println!("step {}, {:?}", i + 1, polymer_map);
-        // while cur < polymer.len() - 1 {
-        //     let key = format!("{}{}", polymer[cur],polymer[cur + 1]);
-        //     if insertions.contains_key(&*key) {
-        //         match insertions.get(&*key) {
-        //             Some(&v) => {
-        //                 let inserted_char = v.chars().next().unwrap();
-        //                 polymer_copy.insert(cur + inserts + 1, inserted_char);
-        //                 let key1 = format!("{}{}", polymer[cur], inserted_char);
-        //                 let key2 = format!("{}{}", inserted_char, polymer[cur + 1]);
-        //                 let insert_key = key.clone();
-        //                 *polymer_map.entry(key).or_insert(0) -= 1;
-        //                 *inserted_map.entry(v.to_string()).or_insert(0) += 1;//polymer_map.get(&insert_key).unwrap();
-        //                 *polymer_map.entry(key1).or_insert(0) += 1;
-        //                 *polymer_map.entry(key2).or_insert(0) += 1;
-        //             },
-        //             None => ()
-        //         }
-        //         inserts += 1;
-        //     }
-        //     cur += 1;
-        // }
-        // polymer = polymer_copy.clone();
     }
 
-    println!("{:?}", inserted_map);
-    println!("{:?}", polymer_map);
-
-    // let mut unique_count_map = polymer
-    //     .iter()
-    //     .fold(HashMap::new(), |mut acc, x| {
-    //         *acc.entry(x).or_insert(0) += 1;
-    //         acc
-    //     });
-    // println!("{:?}", unique_count_map);
     let mut unique_count_vec = inserted_map
         .iter()
         .collect::<Vec<_>>();
