9ee7e81d112b43f4a184fd99544eca7a8f1452c8
ragnarok56 <kevin.nacios@gmail.com>
2022-12-03 21:29:11 -0500

    day 3 part 2 and cleanup

--- 2022/03/src/main.rs
+++ 2022/03/src/main.rs
@@ -1,40 +1,75 @@
 use std::{fs, collections::HashSet};
 
+fn priority_score(x: &char) -> u32 {
+    // determine score based on character
+    // a-z = 1 - 26, A-Z = 26-52
+    match x {
+        'a'..='z' => *x as u32 - 96,
+        'A'..='Z' => *x as u32 - 64 + 26,
+        _ => 0
+    }
+}
+trait HashSetChars {
+    fn chars_to_hashset(&self) -> HashSet<char>;
+}
+
+impl HashSetChars for str {
+    // get unique set of characters in a string
+    fn chars_to_hashset(&self) -> HashSet<char> {
+        self.chars().fold(HashSet::new(), |mut acc, x| {
+            acc.insert(x);
+            acc
+        })
+    }
+}
+
 fn part1() -> u32 {
     fs::read_to_string("in")
         .unwrap()
         .lines()
         .map(|line: &str| {
             let compartments = line.split_at(line.len() / 2);
-            let left_compartment = compartments.0.chars().fold(HashSet::new(), |mut acc, x| {
-                acc.insert(x);
-                acc
-            });
-            let right_compartment = compartments.1.chars().fold(HashSet::new(), |mut acc, x| {
-                acc.insert(x);
-                acc
-            });
+            let left_compartment = compartments.0.chars_to_hashset();
+            let right_compartment = compartments.1.chars_to_hashset();
             left_compartment
                 .intersection(&right_compartment)
-                .map(|x| {
-                    match x {
-                        'a'..='z' => *x as u32 - 96,
-                        'A'..='Z' => *x as u32 - 64 + 26,
-                        _ => 0
-                    }
-                })
+                .map(priority_score)
                 .sum::<u32>()
         })
         .sum()
 }
 
-// fn part2() -> i32{
-//     return 0
-// }
+fn part2() -> u32{
+    let lines: Vec<String> = fs::read_to_string("in")
+        .unwrap()
+        .split('\n')
+        .map(|x| x.to_string())
+        .collect();
+
+    lines
+        .chunks(3)
+        .map(|x| {
+            let sacks = x
+                .iter()
+                .map(|s| s.chars_to_hashset())
+                .collect::<Vec<HashSet<char>>>();
+
+            sacks
+                .iter()
+                .skip(1)
+                .fold(sacks[0].clone(), |acc, s| {
+                    acc.intersection(s).cloned().collect()
+                })
+                .iter()
+                .map(priority_score)
+                .sum::<u32>()
+        })
+        .sum()
+}
 
 fn main() {
     let result = part1();
     println!("{:?}", result);
-    // let result = part2();
-    // println!("{:?}", result);
+    let result = part2();
+    println!("{:?}", result);
 }
