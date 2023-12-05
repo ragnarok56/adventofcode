012c80863f19508aa741a657efda81c5027bc00f
ragnarok56 <kevin.nacios@gmail.com>
2023-12-04 22:00:16 -0500

    day 3

--- 2023/03/src/main.rs
+++ 2023/03/src/main.rs
@@ -1,27 +1,17 @@
 use std::fs::File;
 use std::io::{BufReader, BufRead};
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 #[derive(Debug)]
-struct Coord {
-    x: usize,
-    y: usize
-}
-
-#[derive(Debug)]
-struct EnginePart {
+struct Part {
     value: u32,
-    pos: Vec<Coord>
-}
-
-#[derive(Debug)]
-struct Symbol {
-    value: char,
-    pos: Coord
+    x: usize,
+    start: usize,
+    end: usize,
 }
 
 fn parse_matrix() -> Vec<Vec<char>> {
-    let file = File::open("in_test").expect("file doesnt exist");
+    let file = File::open("in").expect("file doesnt exist");
 
     let reader = BufReader::new(file);
 
@@ -37,61 +27,12 @@ fn parse_matrix() -> Vec<Vec<char>> {
         .collect()
 }
 
-// fn parse_input() -> (Vec<EnginePart>, Vec<Symbol>) {
-//     let file = File::open("in_test").expect("file doesnt exist");
-
-//     let reader = BufReader::new(file);
-
-//     let mut parts: Vec<EnginePart> = Vec::new();
-//     let mut symbols: Vec<Symbol> = Vec::new();
-
-//     let lines = reader
-//         .lines()
-//         .enumerate()
-//         .for_each(|line| {
-//             let x = line.0;
-//             let mut cur_part: Option<&EnginePart> = None;
-//             line.1.unwrap().chars().enumerate().for_each(|c| {
-//                 let y = c.0;
-//                 let pos = Coord{ x: x, y: y };
-//                 match c.1 {
-//                     '.' => {
-//                         if cur_part.is_some() {
-//                             cur_part = None;
-//                         }
-//                     },
-//                     _ => {
-//                         match c.1.is_numeric() {
-//                             true => {
-//                                 let digit = c.1.to_digit(10).unwrap();
-//                                 if cur_part.is_none() {
-//                                     let part = EnginePart{ value: digit, pos: vec![pos]};
-//                                     parts.push(part);
-//                                     cur_part = Some(&part);
-//                                 } else {
-//                                     cur_part.unwrap().value = cur_part.as_mut().unwrap().value * 10 + digit;
-//                                     cur_part.as_mut().unwrap().pos.push(pos);
-//                                 }
-//                             }
-//                             false => {
-//                                 symbols.push(Symbol{ value: c.1, pos: pos});
-//                             }
-
-//                         }
-//                     }
-//                 }
-//             });
-//             cur_part = None
-//         });
-//         (parts, symbols)
-// }
-
 fn get_pos(matrix: &Vec<Vec<char>>, x: usize, y: usize) -> &char {
     matrix.get(x).unwrap().get(y).unwrap()
 }
 
-fn next_to_symbol(matrix: &Vec<Vec<char>>, x: usize, y: usize) -> bool {
-    let mut is_next_to: bool = false;
+fn boundary_check(matrix: &Vec<Vec<char>>, x: usize, y: usize, func: fn(&char) -> bool) -> Vec<(usize, usize)> {
+    let mut matching_positions: Vec<(usize, usize)> = Vec::new();
     for rx in 0..3 {
         for ry in 0..3 {
             if rx + x == 0 {
@@ -108,54 +49,100 @@ fn next_to_symbol(matrix: &Vec<Vec<char>>, x: usize, y: usize) -> bool {
             if cy > matrix.get(x).unwrap().len() - 1 {
                 continue;
             }
-            // println!("checking: {:?}, {:?}", cx, cy);
-            match matrix.get(cx).unwrap().get(cy).unwrap() {
-                '*' | '#' | '$' | '+' => is_next_to = true,
-                _ => ()
+            let val = get_pos(&matrix, cx, cy);
+            if func(val) {
+                matching_positions.push((cx, cy));
             }
         }
     }
-    is_next_to
+    matching_positions
 }
 
-fn print_matrix(matrix: &Vec<Vec<char>>) {
-    for (x, i) in matrix.iter().enumerate() {
-        print!("{:?}|", x);
-        for (y, j) in i.iter().enumerate() {
-            print!("{j}");
+fn next_to_symbol(matrix: &Vec<Vec<char>>, x: usize, y: usize) -> bool {
+    boundary_check(matrix, x, y, |c| !c.is_ascii_digit() && *c != '.').len() > 0
+}
+
+fn gear_positions(matrix: &Vec<Vec<char>>, x: usize, y: usize) -> Vec<(usize, usize)> {
+    boundary_check(matrix, x, y, |c| c.is_ascii_digit())
+}
+
+fn get_parts(matrix: &Vec<Vec<char>>) -> Vec<Part> {
+    let mut part_start_y: Option<usize> = None;
+    let mut part_num: Option<u32> = None;
+    let mut parts: Vec<Part> = Vec::new();
+    for (x, _) in matrix.iter().enumerate() {
+        for (y, _) in matrix.get(x).unwrap().iter().enumerate() {
+            let val = get_pos(&matrix, x, y);
+            if val.is_ascii_digit() {
+                let d = val.to_digit(10).unwrap();
+                // found potential part number, create new or add to current part number
+                if part_num.is_none() {
+                    part_start_y = Some(y);
+                    part_num = Some(d);
+                } else {
+                    part_num = Some(part_num.unwrap() * 10 + d);
+                }
+            } 
+            if y == matrix.get(x).unwrap().len() - 1 || !val.is_ascii_digit() {
+                // reached end of potential part number
+                if part_start_y.is_some() {
+                    // need to check if its a valid part
+                    let range_start = part_start_y.unwrap();
+                    let range_end = y;
+                    let mut is_valid = false;
+                    for i in range_start..range_end {
+                        if next_to_symbol(&matrix, x, i) {
+                            is_valid = true;
+                            break; 
+                        }
+                    }
+                    if is_valid {
+                        parts.push(Part{ value: part_num.unwrap(), x, start: part_start_y.unwrap(), end: y})
+                    }
+                    part_start_y = None;
+                    part_num = None;
+                    // println!()
+                }
+            }
         }
-        println!();
+        // reset each row
+        part_start_y = None;
+        part_num = None;
     }
-}
-
-fn get_part(matrix: &Vec<Vec<char>>, x: usize, y: usize) -> usize {
-    for i in matrix.get(x).unwrap().len() {
-        matrix.get(x).
-    }
+    parts
 }
 
 
 fn main() {
 
     let matrix = parse_matrix();
-    let visited: HashMap<(usize,usize), bool> = HashMap::new();
-    let parts: Vec<i32> = Vec::new();
-    print_matrix(&matrix);
+    let parts = get_parts(&matrix);
 
-    for (x, i) in matrix.iter().enumerate() {
-        for (y, j) in i.iter().enumerate() {
-    for (x, i) in matrix.iter().enumerate() {
-        for (y, j) in i.iter().enumerate() {
-            if get_pos(&matrix, x, y).is_alphanumeric() {
-                let is_next_to_symbol = next_to_symbol(&matrix, x, y);
-                if is_next_to_symbol {
+    println!("p1: {:?}",parts.iter().map(|x| x.value).sum::<u32>());
 
-                    println!("{:?}, {:?}", x, y);
-
+    let mut ratios = 0;
+    let mut gears: HashSet<u32> = HashSet::new();
+    for (i, _) in matrix.iter().enumerate() {
+        for (j, _) in matrix.get(i).unwrap().iter().enumerate() {
+            if *get_pos(&matrix, i, j) == '*' {
+                let positions = gear_positions(&matrix, i, j);
+                for pos in positions {
+                    for part in parts.iter() {
+                        if part.x == pos.0 && part.start <= pos.1 && part.end >= pos.1 {
+                            gears.insert(part.value);
+                            break;
+                        }
+                    }
                 }
+                if gears.len() == 2 {
+                    ratios += gears.iter().fold(1, |mut acc, x| {
+                        acc = acc * x;
+                        acc
+                    })
+                }
+                gears.clear();
             }
         }
-        // println!("{:?}", next_to_symbol(&matrix, 0, 1));
     }
-    println!("{:?}",matrix);
+    println!("p2: {:?}", ratios)
 }
